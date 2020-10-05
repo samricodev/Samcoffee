b80a2f587f3a27ab3de8c8923a54c9a600bd163e
Samuel Rico <sricoe6.8@dgb.email>
2020-10-05 16:45:57 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function getData(){
+    let correo = document.getElementById("email").value;
+    if(correo == ""){
+        document.getElementById("email").focus();
+    } else{
+        document.getElementById("email").value = "";
+        document.getElementById("email").focus();
+    }
+    console.log(correo);
+}
+
+function Aviso(){
+    alert("Esperanos poquito!");
+}
